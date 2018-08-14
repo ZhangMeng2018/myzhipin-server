bb9ee3e0cd9dcbec2157d40f8b3948ed5333ab09
Zhang Meng <970092132@qq.com>
2018-08-15 07:22:38 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.post('/register', function(req, res) {
     }else{
       new UserModel({username,password:md5(password),type}).save((err,userDoc) =>{
         res.cookie('userid', userDoc._id, {maxAge: 1000*60*60*24*7});
-        res.send({code: 0, data: userDoc})
+        res.send({code: 0, data: {_id: userDoc._id, username, type}})
       });
     }
   })
@@ -34,4 +34,31 @@ router.post('/login', function(req, res) {
   })
 });
 
+router.post('/updateUser', function(req, res) {
+  const userid = req.cookies.userid;
+  if(!userid){
+   return res.send({code:1,msg:'请先登录'})
+  }
+  UserModel.findByIdAndUpdate({_id:userid},req.body,(err,userDoc) =>{
+    if(!err){
+      const {username,type,_id} = userDoc;
+      res.send({code: 0, data: {username,type,_id,...req.body}})
+    }else{
+      res.send({code:1,msg:'请先登录'})
+    }
+  })
+});
+
+router.get('/user', function(req, res) {
+  const userid = req.cookies.userid;
+  UserModel.findOne({_id:userid},{password:0},(err,userDoc) =>{
+    if(!userDoc){
+      res.clearCookie('userid');
+      res.send({code:1,msg:'请先登录'})
+    }else{
+      console.log(userDoc)
+      res.send({code: 0, data: userDoc})
+    }
+  })
+});
 module.exports = router;
