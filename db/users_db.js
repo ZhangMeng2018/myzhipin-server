0cd9cb81ce4a73f791bca3d39cabdf35ecfe4d57
Zhang Meng <970092132@qq.com>
2018-08-18 20:55:20 +0800

    123

--- db/users_db.js
+++ db/users_db.js
@@ -23,7 +23,7 @@ const chatSchema =mongoose.Schema({
   to: {type: String, required: true}, // 接收用户的id
   chat_id: {type: String, required: true}, // from和to组成的字符串
   content: {type: String, required: true}, // 内容
-  read: {type:Boolean, default: false}, // 标识是否已读
+  unRead: {type:Number, default: 1}, // 标识是否已读
   create_time: {type: Number} // 创建时间
 });
 // 定义能操作chats集合数据的Model
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const md5 = require('blueimp-md5');
 
-const {UserModel} = require('../db/users_db');
+const {UserModel,ChatModel} = require('../db/users_db');
 
 const router = express.Router();
 const filter = {password:0,__v:0};
@@ -57,21 +57,40 @@ router.get('/user', function(req, res) {
       res.clearCookie('userid');
       res.send({code:1,msg:'请先登录'})
     }else{
-      console.log(userDoc);
       res.send({code: 0, data: userDoc})
     }
   })
 });
 router.get('/userList', function(req, res) {
   const type = req.query.type;
-  console.log(type,req.query);
   UserModel.find({type},filter,(err,userList) =>{
     if(!userList.length){
       res.send({code:1,msg:'暂无用户在线'})
     }else{
-      console.log(userList.length);
       res.send({code: 0, data: userList})
     }
   })
 });
+
+router.get('/msglist', function(req, res) {
+  const userid = req.cookies.userid;
+  ChatModel.find({'$or':[{from:userid},{to:userid}]},filter,(err,chatMsgs) =>{
+    if(!chatMsgs){
+      res.send({code:1,msg:''})
+    }else{
+      res.send({code: 0, data: chatMsgs})
+    }
+  })
+});
+
+router.post('/readmsg',function (req,res) {
+  const to = req.cookies.userid;
+  const from = req.body.from;
+  console.log(req.body,'更新数据库');
+  ChatModel.update({to,from,unRead:1},{$set:{unRead: 0}},{multi: true},(err,doc)=>{
+    res.send({code: 0, data: doc.nModified})
+  })
+});
+
+
 module.exports = router;
